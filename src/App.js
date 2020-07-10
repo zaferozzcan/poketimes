7fd9eb50c12e8b7cbf0003d744449e7ab0960976
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-07-10 18:26:10 -0400

    Add About and Contact components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 // import "./styles.css";
 import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        aliquid cum consequatur culpa tenetur aut ut. Voluptate quae quis rem
+        quia ut hic, dignissimos laborum accusamus, nam ducimus commodi
+        voluptates?
+      </p>
+    </div>
+  );
+}
+export default Contact;
